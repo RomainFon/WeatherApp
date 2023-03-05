67e714bbba47597a0dd0cad82a2b7449b229cde5
RomainFon <romain.fon974@gmail.com>
2023-03-05 01:02:19 +0100

    :rocket: Deploy PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import {VitePWA} from 'vite-plugin-pwa'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true
+            },
+            manifest: {
+                scope: '/',
+                name: 'Weather App',
+                short_name: 'Weather App',
+                description: 'Weather Application',
+                theme_color: '#ffffff',
+                background_color: '#bae6fd',
+                icons: [
+                    {
+                        src: 'android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'maskable_icon_x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
